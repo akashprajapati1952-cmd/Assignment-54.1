8e358d0a5d8136375769d82b3a36c3d49d1047b3
akashprajapati1952-cmd <akashprajapati1952@gmail.com>
2026-02-12 22:03:46 +0530

    header m changed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 function Footer(data) {
   return(
-    <div class="flex bg-gray-500 h-[5vh] w-full items-center justify-between px-20 py-5">
+    <div class="flex bg-gray-500 h-[5vh] w-full items-center justify-between px-5 sm:px-20 py-5">
       <p class="text-xs">Copyright © Codeyogi</p>
       <p class="text-xs">Powered by Codeyogi</p>
     </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 function Header(data) {
   return(
-    <div class="flex w-[100vw] h-[5vh] items-center justify-start px-20 py-5 bg-white">
+    <div class="flex w-[100vw] h-[5vh] items-center justify-start px-5 sm:px-20 py-5 bg-white">
       <img class="w-20" src={data.path} alt="_logo"></img>
     </div>
   );
